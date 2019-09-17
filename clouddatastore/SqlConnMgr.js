cb791f15f8bb0b856da50fe9a03c5d7e12cb713a
Indunil1992 <indunil@adroitlogic.com>
2019-09-17 17:00:33 +0530

    RUN QUARY

--- clouddatastore/SqlConnMgr.js
+++ clouddatastore/SqlConnMgr.js
@@ -4,7 +4,7 @@ module.exports = function() {
     this.dbConnections["instance10"] = {
         host: "34.74.106.203",
         port: "3306",
-        user: "mysql.sys",
+        user: "root",
         password: process.env.Password_sqlinstance10,
         database: "information_schema"
     };
--- clouddatastore/function.js
+++ clouddatastore/function.js
@@ -3,14 +3,21 @@ let sqlConnMgr = require('./SqlConnMgr');
 const sql = new SL_GCP.SQL(sqlConnMgr);
 
 exports.handler = function (request, response) {
+    // You can pass the existing connection to this function.
+    // A new connection will be created if it's not present as the third param.
     // You must always end/destroy the DB connection after it's used.
-    sql.beginTransaction({
-        instanceIdentifier: 'instance10'
-    }, function (error, connection) {
+    sql.query({
+        instanceIdentifier: 'instance10',
+        query: 'INSERT INTO DATA(ID,NAME,AGE)',
+        inserts: [12, "TEST123", 22]
+    }, function (error, results, connection) {
         if (error) {
             throw error;
+        } else {
+            // use `results` to do your magic
         }
+        connection.end();
     });
-connection.end();
+
     response.send({ "message": "Successfully executed" });
 }
